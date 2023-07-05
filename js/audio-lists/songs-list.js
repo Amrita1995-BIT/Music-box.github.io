292d5f65f044841306d3463ad169d1b946e93695
Amrita1995-BIT <114817367+Amrita1995-BIT@users.noreply.github.com>
2023-07-05 23:19:15 +0530

    Add files via upload

---
+++ js/audio-lists/songs-list.js
@@ -0,0 +1,78 @@
+let BASE_URL_SONGS = './resources/audio/music/songs';
+let BASE_URL_IMAGES = './resources/audio/music/images';
+
+export const musicLibrary = [
+    {
+        name: "Krishna Flute",
+        path: `${BASE_URL_SONGS}/Krishna.mp3`,
+        img: `${BASE_URL_IMAGES}/erf.jpg`,
+        singer: "Rakesh Chaurasia"
+    },
+    {
+        name: "Ekadantaya",
+        path: `${BASE_URL_SONGS}/Ekadantaya.mp3`,
+        img: `${BASE_URL_IMAGES}/groovyhiphop.jpg`,
+        singer: "Shankar Mahadevan"
+    },
+    {
+        name: "Classical Song",
+        path: `${BASE_URL_SONGS}/Classical.mp3`,
+        img: `${BASE_URL_IMAGES}/energy.jpg`,
+        singer: "Udit Narayan"
+    },
+    {
+        name: "Mere Dholna",
+        path: `${BASE_URL_SONGS}/Meredholna.mp3`,
+        img: `${BASE_URL_IMAGES}/epic.jpg`,
+        singer: "Pritam Chakraborty"
+    },
+    {
+        name: "Relax Music",
+        path: `${BASE_URL_SONGS}/relax.mp3`,
+        img: `${BASE_URL_IMAGES}/erf.jpg`,
+        singer: "Udit Narayan"
+    },
+    {
+        name: "Shape of you",
+        path: `${BASE_URL_SONGS}/soy.mp3`,
+        img: `${BASE_URL_IMAGES}/groovyhiphop.jpg`,
+        singer: "Indian Style"
+    },
+    {
+        name: "Mohe Rang",
+        path: `${BASE_URL_SONGS}/moherang.mp3`,
+        img: `${BASE_URL_IMAGES}/moose.jpg`,
+        singer: "Birju Maharaj"
+    },
+    {
+        name: "Phir Aur Kya Chahiye Zara Hatke Zara Bachke",
+        path: `${BASE_URL_SONGS}/Phir Aur Kya Chahiye Zara Hatke Zara Bachke.mp3`,
+        img: `${BASE_URL_IMAGES}/Phir Aur Kya Chahiye Zara Hatke Zara Bachke.jpg`,
+        singer: "Arijit Singh"
+    },
+    {
+        name: "Hai Kaisi Kaisi Jubin Nautiyal",
+        path: `${BASE_URL_SONGS}/Hai Kaisi Kaisi Jubin Nautiyal.mp3`,
+        img: `${BASE_URL_IMAGES}/Hai Kaisi Kaisi Jubin Nautiyal.jpg`,
+        singer: "Jubin Nautiyal"
+    },
+    {
+        name: "Kam Toh Nahi Payal Dev",
+        path: `${BASE_URL_SONGS}/Kam Toh Nahi Payal Dev.mp3`,
+        img: `${BASE_URL_IMAGES}/Kam Toh Nahi Payal Dev.jpg`,
+        singer: "Payal Dev, Kunaal Vermaa"
+    },
+    {
+        name: "Kya Loge Tum",
+        path: `${BASE_URL_SONGS}/Kya Loge Tum.mp3`,
+        img: `${BASE_URL_IMAGES}/Kya Loge Tum.jpg`,
+        singer: "Jaani, B Praak"
+    },
+    {
+        name: "Khoobsurat",
+        path: `${BASE_URL_SONGS}/Khoobsurat.mp3`,
+        img: `${BASE_URL_IMAGES}/Khoobsurat.jpg`,
+        singer: "Neha Kakkar, Raghav Chaitanya"
+    },
+
+];
